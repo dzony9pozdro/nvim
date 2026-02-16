531815a34c027348f93dc5a27110c9f117bbe437
dzony9pozdro <jan@tomaka.it>
2026-02-17 00:58:19 +0100

    autopairs keymap implementation

--- init.lua
+++ init.lua
@@ -50,7 +50,13 @@ vim.o.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
-
+--
+-- autopairs
+vim.keymap.set('i', '[', '[]<Left>', { noremap = true })
+vim.keymap.set('i', '{', '{}<Left>', { noremap = true })
+vim.keymap.set('i', '(', '()<Left>', { noremap = true })
+vim.keymap.set('i', "'", "''<Left>", { noremap = true })
+vim.keymap.set('i', '"', '""<Left>', { noremap = true })
 -- Enable break indent
 vim.o.breakindent = true
 
@@ -528,9 +534,7 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
-        clangd = {},
-        gopls = {},
-        basedpyright = {},
+        --basedpyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -549,9 +553,7 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'clangd = {}',
-        'gopls = {}',
-        'basedpyright = {}',
+        --'basedpyright = {}',
         'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
